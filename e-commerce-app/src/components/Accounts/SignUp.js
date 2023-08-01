4e7b84bbf27bcfbf916c4c5b35739694839c2481
Moses Kariuki <moses.kamau@student.moringaschool.com>
2023-08-01 15:24:21 +0300

    db-json file

---
+++ e-commerce-app/src/components/Accounts/SignUp.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+
+function SignUp(){
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h1>Sign Up</h1>
+                <label for="email">Email</label>
+                <input
+                    type='email'
+                    id='email'
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+                <label for="phone">Phone No.</label>
+                <input
+                    type='number'
+                    id='phone'
+                    value={formData.phone}
+                    onChange={handleChange}
+                />
+            <label for="password">Password</label>
+                <input
+                    type='password'
+                    id='password'
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+            </form>
+        </div>
+    )
+}
