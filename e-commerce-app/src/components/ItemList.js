5880e55fafb932cb12e47599beb0a2a6576d8a1a
kipmaiyo <danielkipmaiyo44@gmail.com>
2023-08-01 22:56:33 +0300

    ItemList

--- e-commerce-app/src/components/ItemList.js
+++ e-commerce-app/src/components/ItemList.js
@@ -1,48 +1,28 @@
-
+//import React from "react";
 import React, { useEffect, useState } from 'react';
 import Item from './Item';
 
 const ItemList = () => {
   const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null); // Add error state
 
   useEffect(() => {
-    // Fetch item data from the API
-    fetch('https://')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setItems(data.products);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        setError('Error fetching data. Please try again later.');
-        setIsLoading(false);
-      });
+    // Fetch item data from the API or JSON file
+    fetch('http://localhost:4001/products')
+      .then((response) => response.json())
+      .then((data) => setItems(data)) 
+      .then((data) => console.log(data) )
+
+
+      .catch((error) => console.error('Error fetching data:', error));
   }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
 
   return (
     <div className="item-list">
       {items.map((item) => (
-        <Item key={item.id} item={item} />
+        <Item key={item.name} item={item} />
       ))}
     </div>
   );
 };
 
 export default ItemList;
-
