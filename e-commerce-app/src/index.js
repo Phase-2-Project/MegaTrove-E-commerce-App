ea253a77644189e93b61941a42d4197c171ff4b5
whiplade <nmawira13@gmail.com>
2023-08-02 21:32:22 +0300

    auth-kit

--- e-commerce-app/src/index.js
+++ e-commerce-app/src/index.js
@@ -2,26 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AuthProvider } from 'react-auth-kit';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { AuthProvider } from 'react-auth-kit';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from './components/Accounts/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider
+    {/* <AuthProvider
     authType={"cookie"}
     authName={"_auth"}
     cookieDomain={window.location.hostname}
     cookieSecure={false}
-    >
-      <BrowserRouter>
+    > */}
+      {/* <BrowserRouter> */}
         <App />
-        <Routes>
-          <Route path="/Accounts" element={<SignUp />} />
-        </Routes>
-     </BrowserRouter>
-    </AuthProvider>
+        {/* <Routes> */}
+          {/* <Route path="/Accounts" element={<SignUp />} /> */}
+        {/* </Routes> */}
+     {/* </BrowserRouter> */}
+    {/* </AuthProvider> */}
   </React.StrictMode>
 );
 
