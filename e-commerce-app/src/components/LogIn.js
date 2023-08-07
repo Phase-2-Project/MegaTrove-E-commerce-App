f0e616b565c2bc404b4aa5b69b82bedd6f8373e1
karis-m <moses.kamau@student.moringaschool.com>
2023-08-07 03:29:14 -0400

    Login

--- e-commerce-app/src/components/LogIn.js
+++ e-commerce-app/src/components/LogIn.js
@@ -1,48 +1,3 @@
-
-<<<<<<< HEAD
-import React, {useEffect, useState} from "react";
-import NavBar from "./NavBar";
-function LogIn(){
-    const [signedUpData, setSignedUpData] = useState([])
-    
-    const [formData, setFormData] = useState({
-        username: "",
-        password:""
-    })
-
-        useEffect(()=> {fetch('http://localhost:3001/users', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            })
-            
-            .then(response => response.json())
-            .then(data => setSignedUpData(data))
-            .catch(error => console.log(error))
-            // setIsLoggedIn(true)
-            // window.location.assign("/")
-          },[]) 
-
-    
-    const [userConfirmed,setUserConfirmed] = useState(false)
-    const confirmed = signedUpData.map((user) => {
-      // const SignUpDetails =JSON.parse(user)
-      if( user.email === formData.username && user.password === formData.password){
-        setUserConfirmed(true)
-      }
-      else{
-        
-      }
-      return (null)
-    })
-    // console.log(confirmed)
-    
-    
-
-
-    function handleChange(event){
-=======
 import React, { useEffect, useState } from "react";
 import axios from "axios"
 import { NavLink } from "react-router-dom";
@@ -55,11 +10,9 @@ function LogIn({ setIsLoggedIn }) {
         email: "",
         password: ""
     })
-    const [user, setUser] = useState()
-    
-    const handleSubmit = ((e) => {
+    const handleSubmit = ( (e) => {
         e.preventDefault()
-        const user = { email, password };
+  
         fetch('http://localhost:3001/users', {
             method: 'GET',
             headers: {
@@ -91,7 +44,6 @@ function LogIn({ setIsLoggedIn }) {
     
 
     function handleChange(event) {
->>>>>>> e3cbc6a7165dc747f0d385176d1243c7abad28d4
         const key = event.target.id
         const value = event.target.value
         setFormData({
@@ -100,29 +52,6 @@ function LogIn({ setIsLoggedIn }) {
     }
     return (
         <div id="login">
-<<<<<<< HEAD
-      
-        <form onSubmit={HandleSubmit}>
-            <table>
-            <tr><td><label htmlFor="email">Email</label></td>
-                <td><input
-                    type='email'
-                    id='email'
-                    value={formData.email}
-                    onChange={handleChange}
-                /></td></tr>
-                
-            <tr><td><label htmlFor="password">Password</label></td>
-                <td><input
-                    type='password'
-                    id='password'
-                    value={formData.password}
-                    onChange={handleChange}
-                /></td></tr>
-                <tr><td><input type="submit" value="Log in"/></td></tr>
-            </table>
-        </form>
-=======
 
             <form onSubmit={handleSubmit}>
                 <table>
@@ -147,7 +76,6 @@ function LogIn({ setIsLoggedIn }) {
                     </NavLink></td></tr>
                 </table>
             </form>
->>>>>>> e3cbc6a7165dc747f0d385176d1243c7abad28d4
         </div>
     )
 }
